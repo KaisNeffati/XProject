4e045249dfc0dffecf5abfc2d497a5d188125ed8
KaisNeffati <nef_kais@outlook.fr>
2016-11-22 23:28:13 +0100

    --- Kais Commit ---

--- app/src/main/java/com/example/kaisneffati/maps3/Main2Activity.java
+++ app/src/main/java/com/example/kaisneffati/maps3/Main2Activity.java
@@ -24,6 +24,7 @@ import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
+    // ---- changed line ----
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
      * fragments for each of the sections. We use a
